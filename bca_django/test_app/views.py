acd19905505b36c6b26f2e18846e786e78c37207
bkitor <bkitor@gmail.com>
2020-09-28 08:55:02 -0400

    flake8

--- bca_django/test_app/views.py
+++ bca_django/test_app/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
 @api_view(['GET'])
 def public(request):
     return HttpResponse("You pressed the public Button")
+
 
 @api_view(['GET'])
 def private(request):
