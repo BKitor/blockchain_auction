3cdd9e4df341357b2d1eb79d8f63d23b1115a2f6
bkitor <bkitor@gmail.com>
2020-11-21 10:01:16 -0500

    init with put works

--- bca_django/test_app/apps.py
+++ bca_django/test_app/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from test_app.blockchain import init_blockchain
 
 
 class TestAppConfig(AppConfig):
     name = 'test_app'
-
-    def ready(self):
-        init_blockchain()
--- bca_django/test_app/views.py
+++ bca_django/test_app/views.py
@@ -1,5 +1,6 @@
 import json
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from rest_framework.decorators import api_view
@@ -9,10 +10,11 @@ from rest_framework import generics, permissions, viewsets, status
 from rest_framework.response import Response
 from test_app.serializer import UserSerializer, GroupSerializer, ProfileSerializer, SealedBidSerializer
 from rest_framework.decorators import action
-from test_app.blockchain import sealed_bid_bytecode, sealed_bid_abi, w3
-from django.conf import settings
-# Create your views here.
+from test_app.blockchain import BChain
 
+# Create your views here.
+bchain = BChain()
+w3 = bchain.get_w3()
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -62,7 +64,7 @@ class SealedBidViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class StartAuctionView(generics.GenericAPIView):
+class StartAuctionView(generics.RetrieveAPIView):
     queryset = SealedBid.objects.all()
     serializer_class = SealedBidSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -71,7 +73,7 @@ class StartAuctionView(generics.GenericAPIView):
         auction = self.get_object()
 
         if auction.auction_id != "":
-            return Response({"error": "auction_id is not None, Can't start an auction that's already started","value":auction.auction_id}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "auction_id is not None, Can't start an auction that's already started", "value": auction.auction_id}, status=status.HTTP_400_BAD_REQUEST)
 
         if auction.min_bid is None:
             return Response({"error": "min_bid is None, Can't start an auction that doesn't have a mininum bid"}, status=status.HTTP_400_BAD_REQUEST)
@@ -79,18 +81,23 @@ class StartAuctionView(generics.GenericAPIView):
         if auction.end_time is None:
             return Response({"error": "end_time is None, Can't start an that'll never end"}, status=status.HTTP_400_BAD_REQUEST)
 
-        SealedBid = w3.eth.contract(
-            abi=sealed_bid_abi, bytecode=sealed_bid_bytecode)
 
-        time_limit = auction.end_time - time.now()
+        now = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=5)
+        if(auction.end_time < now):
+            return Response({"error": "end time has passed, the auction has to start before it ends"}, status=status.HTTP_400_BAD_REQUEST)
+
+        time_d = auction.end_time - now
+        time_limit = int(time_d.total_seconds())
         min_bid = auction.min_bid
 
-        tx_hash = SealedBid.constructor(
-            time_limit, auction.owner, min_bid).transact({'from': settings.ADMIN_BLOCKCHAIN_USER})
+        try:
+            owner = Profile.objects.get(user=auction.owner)
+        except Profile.DoesNotExist:
+            return Response({"error": "owner of contract does not exist, the auction needs an owner"}, status=status.HTTP_404_NOT_FOUND)
 
-        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        contract_id = bchain.launch_sealed_bid(time_limit, owner.wallet, min_bid)
 
-        auction.auction_id = tx_receipt.contractAddress
+        auction.auction_id = contract_id
         auction.save()
 
         return Response(SealedBidSerializer(auction, context={'request': request}).data, status=status.HTTP_200_OK)
