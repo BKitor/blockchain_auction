a5b6df21b11a35061733f85bc3d3250ddce6ce31
Ramtin-kalkhoran <16rak3@queensu.ca>
2021-02-03 12:49:01 -0500

    Dutch auction backend

--- bca_django/test_app/models.py
+++ bca_django/test_app/models.py
@@ -63,3 +63,7 @@ def delete_auth_token(sender, instance=None, using=None, **kwargs):
 
 class English(Auction):
     min_bid = models.IntegerField()
+
+
+class Dutch(Auction):
+    min_bid = models.IntegerField()
